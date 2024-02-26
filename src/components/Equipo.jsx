5042bd8b3c16d5850faac616817ae0d2f99ffc81
TOMAS TAPIA <tomastapia@uc.cl>
2024-02-25 23:40:59 -0300

    cambios

--- src/components/Equipo.jsx
+++ src/components/Equipo.jsx
@@ -32,8 +32,9 @@ const people = [
           <div className="max-w-2xl">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl" >Nosotros</h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Encuentra al profesor que siempre estuviste buscando! No dudes en <a href='https://wa.me/56973927173' target="_blank" className="underline">contactarnos</a> para resolver tus dudas.
+              Somos un grupo de <b>exalumnos del Colegio San Ignacio El Bosque</b>, estudiando en las <b>mejores universidades del país</b> y estamos ofreciendo un servicio de <b>clases particulares</b>.
             </p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">Encuentra al profesor que siempre estuviste buscando! No dudes en <a href='https://wa.me/56973927173' target="_blank" className="underline"><b>contactarnos</b></a> para resolver tus dudas.</p>
           </div>
           <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
             {people.map((person) => (
